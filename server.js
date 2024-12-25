61f2d4caf11e46f67ebac15afb26521c3c01c6fc
duy366110 <duy366110@gmail.com>
2024-12-25 09:14:52 +0700

    #Commit change port

--- server.js
+++ server.js
@@ -405,7 +405,7 @@ server.use('/crm/mp', router);
 server.use('/kfm/tvr', router);
 server.use('/kfm/kms', router);
 
-const PORT = 1234;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
